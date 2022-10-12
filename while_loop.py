52b5850e7f5fa9031db7f375ff32fe9a0aa7ccd0
sushilmakaju <sushilmakaju559@gmail.com>
2022-10-12 21:51:43 +0600

    multiplication of table

--- while_loop.py
+++ while_loop.py
@@ -1,14 +1,24 @@
-#i=1
-#while i<=10:
-   # print("hello world")
-    #i = i+1
+# i=1
+# while i<10:
+#    print("hello world")
+#    i+=1
 
-""""""
-x=0
-i=1
-while i <= 10:
-    x = x+i
-    i=i+1
-    print(x)
+# x=0
+# i=1
+# while i <= 10:
+#     x=x+i
+#     i=i+1
+#     print(x)
+
+
+table =int(input("Enter the table :"))
+start =int(input("Enter the starting number : "))
+limit =int(input("Enter the limit :"))
+while(start<=limit):
+  print(start*table)
+  start=start+1
+
+
+
 
 
