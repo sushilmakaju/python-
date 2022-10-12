bd4629653a4a6291c675b8e17116f91f0f934f87
sushilmakaju <sushilmakaju559@gmail.com>
2022-10-12 20:41:13 +0600

    fibonacci series

--- exercise_fibonaci series.py
+++
@@ -1,6 +0,0 @@
-
-a = 0
-b = 1
-while b<50 :
-    print(b)
-    a, b = b, a+b
---
+++ fibonaci_series ex.py
@@ -0,0 +1,7 @@
+#Write a Python program to get the Fibonacci series between 0 to 50.
+
+a = 0
+b = 1
+while b<50 :
+    print(b)
+    a, b = b, a+b
